46370d422c703af952f9136553366352269dbd2f
jvarela-zenika <55753127+jvarela-zenika@users.noreply.github.com>
2020-01-22 10:26:26 +0100

    start

---
+++ src/main/java/com/jvarela/mockupsgenerator/MockupsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.jvarela.mockupsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockupsGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockupsGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jvarela/mockupsgenerator/MockupsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvarela.mockupsgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockupsGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
